2464514559a7ca05c6c9f0cd897e40af214c582e
Karthik Dutt <karthickdutt@gmail.com>
2021-11-11 18:29:58 +0530

    Added smart contract function

--- utilities/DeploymentApplicationExample.py
+++ utilities/DeploymentApplicationExample.py
@@ -11,11 +11,20 @@ from algosdk import account, mnemonic, logic
 from algosdk.v2client import algod
 from pyteal import *
 
+tmpl_timeout= Int(17826590)
+tmpl_fee = Int(1000)
+tmpl_period = Int(1) 
+tmpl_dur = Int(1) # Since this would be a One time transaction after timeout 
+tmpl_lease = Bytes("base64", "023sdDE2")
+tmpl_amt = Int(2000)
+tmpl_rcv = Addr("YDUVA6WMJAFVOF5GEVHGHHTMMKOE5MCVS6OIDZ4S5XKLQPCEZ3ZB7URJQ4") #Reciever address
+tmpl_timeout = Int(30000) 
+
 # user declared account mnemonics
-creator_mnemonic = "finger rigid hat room course salmon say detect avocado assault awake sea public curious exit valve donkey tired escape dash drink diagram section absent cruise"
+creator_mnemonic = "allow artist school host piano six gravity music sail success fancy accuse ginger blouse tired cherry awesome frozen absorb bike cheese speed castle ability chronic"
 # user declared algod connection parameters. Node must have EnableDeveloperAPI set to true in its config
-algod_address = "http://localhost:4001"
-algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+algod_token = "267962e2624ed5c1d4bf8a2ea638fc455812087b288eb044dc6f3a671645310a"
+algod_address = "http://127.0.0.1:8080"
 
 # helper function to compile program source
 def compile_program(client, source_code):
@@ -140,6 +149,12 @@ def approval_program():
     # Mode.Application specifies that this is a smart contract
     return compileTeal(program, Mode.Application, version=3)
 
+def smart_contract_program(tmpl_fee=tmpl_fee,tmpl_period=tmpl_period,tmpl_dur=tmpl_dur,tmpl_lease=tmpl_lease,tmpl_amt=tmpl_amt,tmpl_rcv=tmpl_rcv,tmpl_timeout=tmpl_timeout):
+	periodic_pay_core = And(Txn.type_enum() == TxnType.Payment,Txn.fee() < tmpl_fee,Txn.first_valid() % tmpl_period == Int(0),Txn.last_valid() == tmpl_dur + Txn.first_valid(),Txn.lease() == tmpl_lease)
+	periodic_pay_close = And(Txn.close_remainder_to() == tmpl_rcv,Txn.rekey_to() == tmpl_rcv,Txn.receiver() == tmpl_rcv,Txn.first_valid() == tmpl_timeout,Txn.amount() == tmpl_amt,)
+	periodic_pay_escrow = periodic_pay_core.And(periodic_pay_close)
+    return compileTeal(periodic_pay_escrow, Mode.Application, version=3)
+
 def clear_state_program():
     program = Return(Int(1))
     # Mode.Application specifies that this is a smart contract
@@ -203,6 +218,7 @@ def call_app(client, private_key, index, app_args) :
 
     print("Application called")
 
+
 def main() :
     # initialize an algodClient
     algod_client = algod.AlgodClient(algod_token, algod_address)
@@ -222,7 +238,10 @@ def main() :
     with open("./approval.teal", "w") as f:
         approval_program_teal = approval_program()
         f.write(approval_program_teal)
-
+    
+    with open("./smart_contract.teal", "w") as f: #Write smart contract teal to file
+        smart_contract_teal = smart_contract_program()
+        f.write(smart_contract_teal)
 
     # compile program to TEAL assembly
     with open("./clear.teal", "w") as f:
@@ -231,6 +250,7 @@ def main() :
 
     # compile program to binary
     approval_program_compiled = compile_program(algod_client, approval_program_teal)
+    smart_contract_compiled = compile_program(algod_client, smart_contract_teal)# Added - Compile smart program
 
     # compile program to binary
     clear_state_program_compiled = compile_program(algod_client, clear_state_program_teal)
@@ -239,7 +259,7 @@ def main() :
     print("Deploying Counter application......")
 
     # create new application
-    app_id = create_app(algod_client, creator_private_key, approval_program_compiled, clear_state_program_compiled, global_schema, local_schema)
+    app_id = create_app(algod_client, creator_private_key, approval_program_compiled, clear_state_program_compiled, global_schema, local_schema) #Replace approval_program_compiled with Smart contract compiled. 
 
     # read global state of application
     print("Global state:", read_global_state(algod_client, account.address_from_private_key(creator_private_key), app_id))
